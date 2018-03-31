61bfca4d92f983dc2cde58cf4dbb343635f3bc70
13051757531 <34478605+13051757531@users.noreply.github.com>
2018-03-31 15:22:34 +0800

    Add files via upload

---
+++ Java__Unit/src/www/Atest/one/Das.java
@@ -0,0 +1,19 @@
+package www.Atest.one;
+
+
+import org.junit.Test;
+
+public class Das {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dd");
+	}
+	
+	@Test
+	public void jjj() {
+		// TODO Auto-generated method stub
+		System.out.println("ssss");
+	}
+
+}
